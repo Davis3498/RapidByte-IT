223065e00fada9abf8fc5e864cbdf156c24c8bb6
Davis3498 <63675214+Davis3498@users.noreply.github.com>
2026-05-02 14:30:41 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const siteNav = document.getElementById("siteNav");
-
-if (menuToggle && siteNav) {
-  menuToggle.addEventListener("click", () => {
-    siteNav.classList.toggle("open");
-  });
-}
